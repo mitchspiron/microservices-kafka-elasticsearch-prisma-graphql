32e3b338bda3b15601298a739f6b36fd7a996653
mitchspiron <mitchspiron280@gmail.com>
2025-10-31 23:04:59 +0300

    feat(post-service): implement CreatePostUseCase for post creation and event publishing

---
+++ post-service/src/application/usecases/CreatePostUseCase.ts
@@ -0,0 +1,26 @@
+import { create } from "domain";
+import { CreatePostInput } from "../../domain/entities/Post";
+import { IPostRepository } from "../../domain/repositories/IPostRepository";
+import { IEventPublisher } from "../../domain/services/IEventPublisher";
+
+export class CreatePostUseCase {
+  constructor(
+    private readonly postRepository: IPostRepository,
+    private readonly eventPublisher: IEventPublisher
+  ) {}
+
+  async execute(input: CreatePostInput) {
+    const post = await this.postRepository.create(input);
+
+    // Publish an event after creating the post
+    await this.eventPublisher.publish("PostCreated", {
+      id: post.id,
+      title: post.title,
+      content: post.content,
+      authorId: post.authorId,
+      createdAt: post.createdAt,
+    });
+
+    return post;
+  }
+}
